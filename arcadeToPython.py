fbce98be679818a851cc4a72f6b95901198ef251
Rayan <154925122+Rayanlupo@users.noreply.github.com>
2024-07-12 17:56:12 +0200

    Update arcadeToPython.py

--- arcadeToPython.py
+++ arcadeToPython.py
@@ -1,6 +1,8 @@
+#required libraries
 import requests
 import gspread
 from google.oauth2.service_account import Credentials
+import time
 scopes = [
     "https://www.googleapis.com/auth/spreadsheets",
 ]
@@ -8,17 +10,25 @@ scopes = [
 
 credentials = Credentials.from_service_account_file("credentials.json", scopes=scopes)
 client = gspread.authorize(credentials)
-token = "213edb5c-0a84-49f6-be66-f18d31e22400"
+token = "YOUR_API_TOKEN_HERE"
 header = {'Authorization': f'Bearer {token}'}
-response = requests.get("https://hackhour.hackclub.com/api/history/U074C9BE604", headers=header)
+response = requests.get("https://hackhour.hackclub.com/api/history/YOUR_SLACK_ID", headers=header)
 json_response = response.json()
 data_list = json_response.get("data", [])
 i = len(data_list) - 1  
-row = 1
-sheet_id = "145fClGtHKTutKw0MtmuzNLdNBBoGQcgutOUJK9ry3JE"
+row = 2  #row 1 is for the headers
+
+sheet_id = "YOUR_SHEET_ID_HERE( you find it in the URL)"
 sheet = client.open_by_key(sheet_id)
 worksheet= sheet.sheet1
 values_list = sheet.sheet1.row_values(1)
+
+#Headers
+worksheet.update_acell(f"A1","Name")
+worksheet.update_acell(f"B1","Date")
+worksheet.update_acell(f"C1","Goal")
+worksheet.update_acell(f"D1","Minutes")
+worksheet.update_acell(f"E1","Progres Bar")
 
 
 
@@ -30,23 +40,48 @@ while i > 0:
             name = data_list[i].get("work")
             print(name)
             worksheet.update_acell(f"A{row}",name)
+            time.sleep(1) # 1 second pause to prevent API rate limit
       
         #date
         if "createdAt" in data_list[i]:
+            #get date
             date = data_list[i].get("createdAt")
-        
+            #format date
             date = date[:10]
             date = date.split("-")
-            date = f"{date[1]}/{date[2]}/{date[0]}"
+            date = f"{date[2]}/{date[1]}/{date[0]}"
             print(date)
+            #update date in the sheet
             worksheet.update_acell(f"B{row}",date)
-       
-        if "ended" in data_list[i]:
-            end = data_list[i].get("ended")
-            worksheet.update_acell(f"B{row}",end)
+            time.sleep(1)
             
-            print(end)
+        #goal
+        if "goal" in data_list[i]:
+            #get goal
+            goal = data_list[i].get("goal")
+            print(goal)
+            #check if goal is "No Goal"
+            if goal == "No Goal":
+                goal = "/"
+            else: 
+                goal = goal
+            #update goal in the sheet
+            worksheet.update_acell(f"C{row}",goal)
+            time.sleep(1)
+        #minutes
+        if  "elapsed" in data_list[i]:
+            #get minutes
+            minutes = data_list[i].get("elapsed")
+            print(minutes)
+            #update minutes in the sheet
+            worksheet.update_acell(f"D{row}",minutes)
+            time.sleep(1)
+            #progress bar
+            formule = f'=SPARKLINE(D{row}; {{"charttype" \ "bar"; "max" \ 60}})'
+            worksheet.update_acell(f"E{row}",formule)
+        #increment row and decrease i
         row = row + 1
         i = i - 1
+    #if the response is not successful
     else: 
         print("Error ", response.status_code)
